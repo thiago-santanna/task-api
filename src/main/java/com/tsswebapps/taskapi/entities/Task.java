5dc0567b0b26406489f78fa9b9506b2ae6aca68c
xthiagosant <uzmaki_thiago@outlook.com>
2022-03-31 10:46:14 -0300

    criado classe para entidade da tabela

--- src/main/java/com/tsswebapps/taskapi/entities/Task.java
+++ src/main/java/com/tsswebapps/taskapi/entities/Task.java
@@ -1,6 +1,30 @@
 package com.tsswebapps.taskapi.entities;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tasks")
 public class Task {
-
+	
+	@Id
+	@GeneratedValue
+	private UUID id;
+	
+	@Column(nullable = false)
+	private String description;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
 }
